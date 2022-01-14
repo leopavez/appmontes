c289bf144b7a9807f58de6e5b40dcd553b744838
Leandro Pavez <leandropavez@MacBook-Pro-de-Leandro.local>
2022-01-14 16:21:01 -0300

    recordar usuario y password

--- app/src/main/java/cl/app/montes/view/login/LoginActivity.java
+++ app/src/main/java/cl/app/montes/view/login/LoginActivity.java
@@ -1,6 +1,8 @@
 package cl.app.montes.view.login;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -8,6 +10,7 @@ import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SwitchCompat;
 
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
@@ -26,22 +29,28 @@ public class LoginActivity extends AppCompatActivity {
     TextInputLayout tlemail;
     TextInputLayout tlpassword;
     Button ingresar;
+    SwitchCompat switchCompat;
     InputValidation inputValidation;
     DatabaseHelper myDB;
-
+    SharedPreferences preferences;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getSupportActionBar().hide();
         setContentView(R.layout.activity_login);
+        preferences = getSharedPreferences("recordar", Context.MODE_PRIVATE);
+        boolean stateSwitch = preferences.getBoolean("stateSwitch", false);
         email = (TextInputEditText)findViewById(R.id.txtemail);
         tlemail = (TextInputLayout)findViewById(R.id.textinputLayoutusers);
         password = (TextInputEditText)findViewById(R.id.txtpassword);
         tlpassword = (TextInputLayout)findViewById(R.id.textInputLayoutpass);
+        switchCompat = (SwitchCompat)findViewById(R.id.switch_recordar);
         ingresar = (Button)findViewById(R.id.btniniciar);
         inputValidation = new InputValidation(this);
         myDB = new DatabaseHelper(this);
+        switchCompat.setChecked(stateSwitch);
 
+        cargarChecked();
 
         ingresar.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -60,11 +69,37 @@ public class LoginActivity extends AppCompatActivity {
         }
         if (myDB.LoginUsers(email.getText().toString().trim(), password.getText().toString().trim(), LoginActivity.this)){
 
+            if(switchCompat.isChecked()){
+                SharedPreferences.Editor editor = preferences.edit();
+                editor.putString("email", email.getText().toString());
+                editor.putString("password", password.getText().toString());
+                editor.putBoolean("stateSwitch", switchCompat.isChecked());
+                editor.commit();
+            }else{
+                SharedPreferences.Editor editor = preferences.edit();
+                editor.putString("email","");
+                editor.putString("password", "");
+                editor.putBoolean("stateSwitch", switchCompat.isChecked());
+                editor.commit();
+            }
+
+
              Intent intent = new Intent(LoginActivity.this, MenuDrawerActivity.class);
              startActivity(intent);
              finish();
         } else{
             Toast.makeText(this, "Usuario o password incorrectos", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void cargarChecked(){
+        SharedPreferences preferences = getSharedPreferences("recordar", Context.MODE_PRIVATE);
+        String emailrecord = preferences.getString("email", "");
+        String passwordrecord = preferences.getString("password","");
+
+        if(!emailrecord.toString().equals("") && !passwordrecord.toString().equals("")){
+            email.setText(emailrecord);
+            password.setText(passwordrecord);
         }
     }
 }
