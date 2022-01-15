8a499e4cbe2beb2d5cbce3e4f1f37e140a960eda
Leandro Pavez <leandropavez.lp@gmail.com>
2022-01-15 19:29:37 -0300

    envio asyncrono success

--- app/src/main/java/cl/app/montes/view/drawer/fragment/Listado_fragment.java
+++ app/src/main/java/cl/app/montes/view/drawer/fragment/Listado_fragment.java
@@ -4,15 +4,19 @@ import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
 
 import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
@@ -29,8 +33,13 @@ import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.Toast;
 
+import com.android.volley.DefaultRetryPolicy;
+import com.android.volley.Request;
 import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.baoyz.swipemenulistview.SwipeMenu;
 import com.baoyz.swipemenulistview.SwipeMenuCreator;
 import com.baoyz.swipemenulistview.SwipeMenuItem;
@@ -39,9 +48,16 @@ import com.loopj.android.http.AsyncHttpClient;
 import com.loopj.android.http.AsyncHttpResponseHandler;
 import com.loopj.android.http.RequestParams;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.nio.charset.StandardCharsets;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
 
 import cl.app.montes.R;
+import cl.app.montes.clases.Usuarios;
 import cl.app.montes.db.DatabaseHelper;
 import cl.app.montes.view.detalle.DetalleActivity;
 import cl.app.montes.view.editar.EditarActivity;
@@ -125,88 +141,9 @@ public class Listado_fragment extends Fragment {
                 NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
 
                 if (networkInfo != null && networkInfo.isConnected()){
-                    progressDialog = new ProgressDialog(getView().getContext());
-                    progressDialog.setTitle("Enviando registros");
-                    progressDialog.setMessage("Espere un momento...");
-                    progressDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
-                    progressDialog.setCancelable(false);
-                    progressDialog.show();
-                    try {
-                        SQLiteDatabase db = myDB.getWritableDatabase();
-                        Cursor cursor = db.rawQuery("SELECT id, id_usuario, fecha, hora, recorrido, productor, producto, " +
-                                "variedad, tara, bandeja, cantidad_envase, kilos_brutos, kilos_netos, total, bandejas_pendientes," +
-                                "bandejas_entregadas, precio_usuario FROM registros", null);
+                    EnviodeDatos enviodeDatos = new EnviodeDatos();
+                    enviodeDatos.execute();
 
-                        while (cursor.moveToNext()) {
-
-                            int id = cursor.getInt(0);
-                            String recorridos_id = cursor.getString(4);
-                            String productor_id = cursor.getString(5);
-                            String productos_id = cursor.getString(6);
-                            String fecha = cursor.getString(2);
-                            String taras_id = cursor.getString(8);
-                            String bandejas_cant = cursor.getString(10);
-                            String kilos_b = cursor.getString(11);
-                            String bandejas_pend = cursor.getString(14);
-                            String bandejas_entre = cursor.getString(15);
-                            String precio_usuario = cursor.getString(16);
-                            String hora = cursor.getString(3);
-
-
-                            final RequestParams params = new RequestParams();
-
-                            params.put("recorridos_id", recorridos_id);
-                            params.put("productor_id", productor_id);
-                            params.put("productos_id", productos_id);
-                            params.put("fecha", fecha);
-                            params.put("taras_id", taras_id);
-                            params.put("bandejas_pendientes", bandejas_pend);
-                            params.put("bandejas_entregadas", bandejas_entre);
-                            params.put("bandejas", bandejas_cant);
-                            params.put("kilos_brutos", kilos_b);
-                            params.put("precio", precio_usuario);
-                            params.put("hora", hora);
-
-                            Handler handler = new Handler(Looper.getMainLooper());
-                            Runnable runnable = new Runnable() {
-                                @Override
-                                public void run() {
-                                    AsyncHttpClient client = new AsyncHttpClient();
-                                    client.post(PROCESAR_URL, params, new AsyncHttpResponseHandler() {
-                                        @Override
-                                        public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
-                                            if (statusCode == 200) {
-                                                Log.i("200", responseBody.toString());
-                                                db.execSQL("DELETE FROM registros WHERE id ='" + id + "'");
-                                                progressDialog.dismiss();
-                                                Toast.makeText(getActivity().getApplicationContext(), "Datos enviados correctamente", Toast.LENGTH_SHORT).show();
-                                                Fragment fragment_actual = new Listado_fragment();
-                                                Fragment fragment = new Listado_fragment();
-                                                getFragmentManager().beginTransaction().remove(fragment_actual).commit();
-                                                getFragmentManager().beginTransaction().replace(R.id.content_frame, fragment).commit();
-                                            }
-                                        }
-
-                                        @Override
-                                        public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
-                                            try {
-                                                String response = new String(responseBody).toUpperCase();
-                                                Log.i("REQUEST FAIL", "" + error.toString() + " AA " + response);
-                                                Toast.makeText(getActivity().getApplicationContext(), "Fallo el envío de datos.", Toast.LENGTH_SHORT).show();
-
-                                            } catch (Exception e) {
-                                                e.printStackTrace();
-                                            }
-                                        }
-                                    });
-                                }
-                            };
-                            handler.post(runnable);
-
-                        }
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                    }
                 }else{
                     Toast.makeText(getActivity().getApplicationContext(), "Error, sin acceso a Internet", Toast.LENGTH_SHORT).show();
                 }
@@ -328,6 +265,112 @@ public class Listado_fragment extends Fragment {
         while (cursor.moveToNext()){
             listaregistros.add(""+cursor.getString(3)+" | "+ ""+cursor.getString(2));
             listaidregistros.add(cursor.getString(0));
+        }
+    }
+
+
+    private class EnviodeDatos extends AsyncTask<Void, Integer, Boolean> {
+        protected Boolean doInBackground(Void... voids) {
+            try {
+                SQLiteDatabase db = myDB.getWritableDatabase();
+                Cursor cursor = db.rawQuery("SELECT id, id_usuario, fecha, hora, recorrido, productor, producto, " +
+                        "variedad, tara, bandeja, cantidad_envase, kilos_brutos, kilos_netos, total, bandejas_pendientes," +
+                        "bandejas_entregadas, precio_usuario, kilos_netos FROM registros", null);
+
+                while (cursor.moveToNext()) {
+
+                    int id = cursor.getInt(0);
+                    String recorridos_id = cursor.getString(4);
+                    String productor_id = cursor.getString(5);
+                    String productos_id = cursor.getString(6);
+                    String fecha = cursor.getString(2);
+                    String taras_id = cursor.getString(8);
+                    String bandejas_cant = cursor.getString(10);
+                    String kilos_b = cursor.getString(11);
+                    String bandejas_pend = cursor.getString(14);
+                    String bandejas_entre = cursor.getString(15);
+                    String precio_usuario = cursor.getString(16);
+                    String hora = cursor.getString(3);
+                    String kilos_netos = cursor.getString(17);
+
+
+                    final RequestParams params = new RequestParams();
+
+                    params.put("recorridos_id", recorridos_id);
+                    params.put("productor_id", productor_id);
+                    params.put("productos_id", productos_id);
+                    params.put("fecha", fecha);
+                    params.put("taras_id", taras_id);
+                    params.put("bandejas_pendientes", bandejas_pend);
+                    params.put("bandejas_entregadas", bandejas_entre);
+                    params.put("bandejas", bandejas_cant);
+                    params.put("kilos_brutos", kilos_b);
+                    params.put("precio", precio_usuario);
+                    params.put("hora", hora);
+                    params.put("kilos_netos", kilos_netos);
+
+                    Handler handler = new Handler(Looper.getMainLooper());
+                    Runnable runnable = new Runnable() {
+                        @Override
+                        public void run() {
+                            AsyncHttpClient client = new AsyncHttpClient();
+                            client.post(PROCESAR_URL, params, new AsyncHttpResponseHandler() {
+                                @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+                                @Override
+                                public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
+                                    if (statusCode == 200) {
+                                        Log.i("200", new String(responseBody,StandardCharsets.UTF_8));
+                                        db.execSQL("DELETE FROM registros WHERE id ='" + id + "'");
+                                    }
+                                }
+
+                                @Override
+                                public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
+                                    try {
+                                        String response = new String(responseBody).toUpperCase();
+                                        Log.i("REQUEST FAIL", "" + error.toString() + " AA " + response);
+                                        Toast.makeText(getActivity().getApplicationContext(), "Fallo el envío de datos.", Toast.LENGTH_SHORT).show();
+
+                                    } catch (Exception e) {
+                                        e.printStackTrace();
+                                    }
+                                }
+                            });
+                        }
+                    };
+                    handler.post(runnable);
+
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            return true;
+        }
+
+        @Override
+        protected void onPreExecute() {
+            progressDialog = new ProgressDialog(getView().getContext());
+            progressDialog.setTitle("Enviando datos");
+            progressDialog.setMessage("Espere un momento...");
+            progressDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+            progressDialog.setCancelable(false);
+            progressDialog.show();
+        }
+
+        protected void onPostExecute(Boolean result) {
+            try {
+                Thread.sleep(3000);
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+            progressDialog.dismiss();
+            Toast.makeText(getActivity().getApplicationContext(),"Envio de datos completado.", Toast.LENGTH_SHORT).show();
+
+            Fragment fragment_actual = new Listado_fragment();
+            Fragment fragment = new Listado_fragment();
+            getFragmentManager().beginTransaction().remove(fragment_actual).commit();
+            getFragmentManager().beginTransaction().replace(R.id.content_frame, fragment).commit();
         }
     }
 }
